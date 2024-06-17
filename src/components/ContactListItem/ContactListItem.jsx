8c18a755b8e1fb6c4a8dd1a45385ba63ef649060
kesobyte <kimangeloalvarez@gmail.com>
2024-06-17 16:09:27 +0800

    updates

---
+++ src/components/ContactListItem/ContactListItem.jsx
@@ -0,0 +1,26 @@
+import PropTypes from 'prop-types';
+import css from './ContactListItem.module.css';
+
+export const ContactListItem = ({ filteredContact, deleteContact }) => {
+  // Handle Delete
+  const handleDelete = () => {
+    deleteContact(filteredContact.id);
+  };
+
+  return (
+    <>
+      <li className={css.list_item}>
+        <p>{filteredContact.name}:</p>
+        <p>{filteredContact.number}</p>
+        <button className={css.deleteBtn} onClick={handleDelete}>
+          Delete
+        </button>
+      </li>
+    </>
+  );
+};
+
+ContactListItem.propTypes = {
+  filteredContact: PropTypes.object.isRequired,
+  deleteContact: PropTypes.func.isRequired,
+};
